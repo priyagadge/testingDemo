d79f69f1879d4f18b63017b925494c66d194e301
user <user@LAPTOP-QOF44QE3>
2022-02-16 13:31:14 +0530

    by priyaG

---
+++ GitDemo/src/GitDemo.java
@@ -0,0 +1,15 @@
+
+public class GitDemo {
+
+	public void m1() {
+	System.out.println("this is method1");
+	}
+	public void m2() {
+		System.out.println("this is method2");
+		}
+	public static void main(String[] args) {
+		GitDemo g=new GitDemo();
+		g.m1();
+		g.m2();
+}
+}
